368e6df4a743da8cba207f13cb88ae483dcda833
Sinisa Ojkic <ojkicsinisa@gmail.com>
2018-04-07 16:53:08 +0200

    Authorise update & delete products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\ProductNotBelongsToUser;
 use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
-use http\Env\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
@@ -85,9 +86,11 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request $request
      * @param  \App\Model\Product $product
      * @return \Illuminate\Http\Response
+     * @throws ProductNotBelongsToUser
      */
     public function update(Request $request, Product $product)
     {
+        $this->ProductUserCheck($product);
         $request['detail'] = $request->description;
         unset($request['description']);
         $product->update($request->all());
@@ -105,8 +108,15 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
+        $this->ProductUserCheck($product);
         $product->delete();
         return response(null, 204);
 
+    }
+
+    public function ProductUserCheck($product){
+        if (Auth::id() !== $product->user_id){
+            throw new ProductNotBelongsToUser;
+        }
     }
 }
